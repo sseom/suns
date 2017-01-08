e867e49a675d657966799b0bea0ebb875b95515e
sseom <ysum1011@gmail.com>
2017-01-09 00:03:29 +0900

    데탑버전일때 gab메뉴 호버영역 수정

--- cjone/js/all.js
+++ cjone/js/all.js
@@ -15,13 +15,12 @@ var depth_1 = document.querySelectorAll('.depth_1');
 var out_focus = document.querySelector('.out_focus');
 
 //마우스 오버, 아웃
-gnb.onmouseover = function(){
+gnb_menu.onmouseover = function(){
   document.styleSheets[0].addRule('.header_wrap::after','display: block');
 };
 
-gnb.onmouseout = function(){
+gnb_menu.onmouseout = function(){
   document.styleSheets[0].addRule('.header_wrap::after','display: none');
-  document.styleSheets[0].addRule('.gnb_wrap::after','display: none');
   for(var k = 0; k < sub_menu.length ; k++){
     sub_menu[k].classList.remove('depth_2');
   }
@@ -36,7 +35,7 @@ gnb.onmouseout = function(){
 for(var i = 0; i < depth_1.length ; i++){
   depth_1[i].onfocus = function(){
     document.styleSheets[0].addRule('.header_wrap::after','display: block');
-    document.styleSheets[0].addRule('.gnb_wrap::after','display: block');
+    document.styleSheets[0].addRule('.gnb_menu::after','display: block');
     for(var l = 0; l < sub_menu.length ; l++){
       sub_menu[l].classList.add('depth_2');
     }
@@ -46,7 +45,7 @@ for(var i = 0; i < depth_1.length ; i++){
 //포커스가 2뎁스 마지막 리스트 아이템을 벗어나면 사라져라
 out_focus.onblur = function(){
   document.styleSheets[0].addRule('.header_wrap::after','display: none');
-  document.styleSheets[0].addRule('.gnb_wrap::after','display: none');
+  document.styleSheets[0].addRule('.gnb_menu::after','display: none');
   for(var k = 0; k < sub_menu.length ; k++){
     sub_menu[k].classList.remove('depth_2');
   }
@@ -67,21 +66,6 @@ total_menu_close.onclick = function(){
   document.styleSheets[0].addRule('.modal_wrap::after','display: none');
   modal_wrap.classList.remove('modal_on');
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
